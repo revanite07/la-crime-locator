102ffc376a76eda34d037690c559f39d6fb1c4b0
Kelan Moffatt <kelanmoffatt07@gmail.com>
2019-03-29 22:12:21 -0700

    heroku fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var logger = require("morgan");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+ar PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
