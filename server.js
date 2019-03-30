8ff411fb5483c128f4d81061e41c3d2522aa794d
Kelan Moffatt <kelanmoffatt07@gmail.com>
2019-03-29 22:14:42 -0700

    h

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var logger = require("morgan");
 // Sets up the Express App
 // =============================================================
 var app = express();
-ar PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
